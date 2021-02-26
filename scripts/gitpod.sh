d25efe51eb8e59dfca1a8c7f47627c3d574f63ca
Ben Chuanlong Du <dclong@users.noreply.github.com>
2021-02-25 16:18:26 -0800

    Update gitpod.sh

--- scripts/gitpod.sh
+++ scripts/gitpod.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 export PATH=/home/gitpod/.local/bin:$PATH
-if [ -f /workspace/blog/main.py ]; then
-    ln -s /workspace/blog/main.py /home/gitpod/.local/bin/blog 
+if [ -f /workspace/blog/blog.py ]; then
+    ln -s /workspace/blog/blog.py /home/gitpod/.local/bin/blog 
 fi
 export EDITOR=vim 
 export PIP_USER=no
